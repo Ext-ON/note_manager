5ad80bccf98691f4f023880361285f9d5690547a
Ext-ON <rychyaks@icloud.com>
2024-12-22 21:02:39 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,11 +1,11 @@
-temp_created_date = "18-12-2024"
-temp_issue_date = "22-12-2024"
-remove_year = input("Скрыть год? (Да/Нет)")
-if remove_year in ["Да","да"]:
+temp_created_date = "30.12.2024"
+temp_issue_date = "31.12.2024"
+remove_year = input('Скрыть год? Введите "да" или "нет"')
+if remove_year.lower() == "да":
     print(temp_created_date[:5:])
     print(temp_issue_date[:5:])
 else:
-        if remove_year in ["Нет","нет"]:
+        if remove_year.lower() == "нет":
             print(temp_created_date)
             print(temp_issue_date)
         else:
